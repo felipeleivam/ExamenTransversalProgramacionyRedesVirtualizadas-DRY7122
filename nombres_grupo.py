53d94965b6d6bf085326b8448de833e7e953a6e1
felipeleivam <felip.leivam@duocuc.cl>
2025-07-18 19:35:16 -0400

    Agrega nombres_grupo.py - con nombres del grupo

---
+++ nombres_grupo.py
@@ -0,0 +1,11 @@
+integrantes = [
+    {"nombre": "Juan Carlos", "apellido": "Arevalo"},
+    {"nombre": "Juan Carlos", "apellido": "Hernandez"},
+    {"nombre": "Felipe", "apellido": "Leiva"},
+    {"nombre": "Samuel", "apellido": "Valdebenito"}
+    # Agrega más si es necesario
+]
+
+print("Integrantes del grupo:")
+for persona in integrantes:
+    print(f"{persona['nombre']} {persona['apellido']}")
